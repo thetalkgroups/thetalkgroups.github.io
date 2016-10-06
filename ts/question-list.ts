cda6c23341ea22819a562d3c405a8ca651b710ed
mathias hansen <mathiashansen@mathiass-MacBook-Pro.local>
2016-10-06 19:44:34 +0200

    added login protections

--- ts/question-list.ts
+++ ts/question-list.ts
@@ -1,21 +1,30 @@
+const createQuestionListItem = (question: Question) => {
+    const item = document.createElement("article")
+
+    item.classList.add("question")
+
+    item.innerHTML = `
+        <header>
+            <p class="question__user-name">${question.user.name}</p>
+            <p class="question__date">${moment(question.date).fromNow()}</p>
+        </header>
+        <a href="question.html?id=${question.id}">${question.title}</a>
+    `
+
+    return item;
+}
+
 window.addEventListener("load", () => {
-    const createQuestionListItem = (question: Question) => {
-        const item = document.createElement("article")
+    const questionListEl = document.querySelector(".question-list") as HTMLElement;
+    const askAQuestion = document.querySelector(".questions-header__ask-a-question") as HTMLElement
 
-        item.classList.add("question")
+    window.userService.user.subscribe(user => {
+        if (!user) return;
 
-        item.innerHTML = `
-            <header>
-                <p class="question__user-name">${question.user.name}</p>
-                <p class="question__date">${moment(question.date).fromNow()}</p>
-            </header>
-            <a href="question.html?id=${question.id}">${question.title}</a>
-        `
+        askAQuestion.hidden = false;
+    })
 
-        return item;
-    }
-
-    const questionListEl = document.querySelector(".question-list");
-
-    getQuestions().map(createQuestionListItem).forEach(item => questionListEl.appendChild(item))
+    getQuestions().then(questions => 
+        questions.map(createQuestionListItem)
+            .forEach(item => questionListEl.appendChild(item)));
 });
--- ts/question.ts
+++ ts/question.ts
@@ -1,31 +1,26 @@
-window.addEventListener("load", () => {
-    const createAnswer = (answer: Answer) => {
-        const item = document.createElement("article");
+const createAnswer = (answer: Answer) => {
+    const item = document.createElement("article");
 
-        item.classList.add("question__answers__answer");
+    item.classList.add("question__answers__answer");
 
-        item.innerHTML = `
-            <img class="question__answers__answer__photo" src="${answer.user.photo}"/>
-            <section class="question__answers__answer__text">
-                <section>
-                    <p class="question__answers__answer__name">${answer.user.name}</p>
-                    <p class="question__answers__answer__date">${moment(answer.date).fromNow()}</p>
-                </section>
-                <p class="question__answers__answer__answer">${answer.answer}</p>
+    item.innerHTML = `
+        <img class="question__answers__answer__photo" src="${answer.user.photo}"/>
+        <section class="question__answers__answer__text">
+            <section>
+                <p class="question__answers__answer__name">${answer.user.name}</p>
+                <p class="question__answers__answer__date">${moment(answer.date).fromNow()}</p>
             </section>
-        `;
+            <p class="question__answers__answer__answer">${answer.answer}</p>
+        </section>
+    `;
 
-        return item;
-    }
-    const missingError = () => {
-        console.error("question missing");
-    }
+    return item;
+}
+const missingError = () => {
+    console.error("question missing");
+}
 
-    const idMatch = location.search.match(/id=(\w+)/);
-    if (!idMatch) return missingError();
-    const question = getQuestion(idMatch[1]);
-    if (!question) return missingError();
-
+window.addEventListener("load", () => {
     const headerPhoto = document.querySelector(".question__header__photo") as HTMLImageElement;
     const headerName = document.querySelector(".question__header__name") as HTMLElement;
     const headerDate = document.querySelector(".question__header__date") as HTMLElement;
@@ -33,13 +28,31 @@ window.addEventListener("load", () => {
     const headerQuestion = document.querySelector(".question__header__question") as HTMLElement;
     const answers = document.querySelector(".question__answers");
     const breadcrumbLocation = document.querySelector(".breadcrumb p") as HTMLElement;
+    const reply = document.querySelector(".question__reply") as HTMLElement;
+    const loginWarning = document.querySelector(".question__login-warning") as HTMLElement;
 
-    headerPhoto.src = question.user.photo;
-    headerName.innerText = question.user.name;
-    headerDate.innerText = moment(question.date).fromNow();
-    headerTitle.innerText = question.title;
-    headerQuestion.innerText = question.question;
-    breadcrumbLocation.innerText = question.title;
+    window.userService.user.subscribe(user => {
+        if (!user) return;
 
-    question.answers.map(createAnswer).forEach(answer => answers.appendChild(answer));
+        loginWarning.hidden = true;
+        reply.hidden = false;
+    })
+
+    const idMatch = location.search.match(/id=(\w+)/);
+    if (!idMatch) return missingError();
+    
+    getQuestion(idMatch[1])
+        .then(question => {
+            
+
+            headerPhoto.src = question.user.photo;
+            headerName.innerText = question.user.name;
+            headerDate.innerText = moment(question.date).fromNow();
+            headerTitle.innerText = question.title;
+            headerQuestion.innerText = question.question;
+            breadcrumbLocation.innerText = question.title;
+
+            question.answers.map(createAnswer).forEach(answer => answers.appendChild(answer));
+        })
+        .catch(missingError);
 });
