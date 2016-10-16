02fb602e04b99d399ed9153bf8ae4472768a0b20
mathias hansen <mathiashansen@mathiass-MacBook-Pro.local>
2016-10-16 14:44:54 +0200

    added "ajax" pagination

--- ts/pagination.ts
+++ ts/pagination.ts
@@ -1,21 +1,48 @@
-export const initPagination = (page: number, numberOfPages: number) => {
+export const initPagination = (page: number, numberOfPages: number, refreshList: (page:number) => any) => {
     const prev = document.querySelector(".pagination__prev") as HTMLAnchorElement;
     const next = document.querySelector(".pagination__next") as HTMLAnchorElement;
     const pageEl = document.querySelector(".pagination__page");
+    const updatePage = () => {
+        if (page === 1)
+            prev.classList.add("disabled");
+        else
+            prev.classList.remove("disabled");
 
-    if (page !== 1) {
-        prev.href = getUrlWithPage(page - 1);
-    }
-    if (page < numberOfPages) {
-        next.href = getUrlWithPage(page + 1);
-    }
+        if (page === numberOfPages)
+            next.classList.add("disabled");
+        else
+            next.classList.remove("disabled");
 
-    pageEl.innerHTML = page + " of " + numberOfPages;
+        history.pushState(null, null, getUrlWithPage(page));
+
+        pageEl.innerHTML = page + " of " + numberOfPages;
+    };
+
+    prev.onclick = () => {
+        if (prev.classList.contains("disabled")) return;
+
+        page -= 1;
+        updatePage();
+        refreshList(page);
+    }
+    next.onclick = () => {
+        if (next.classList.contains("disabled")) return;
+
+        page += 1;
+        updatePage();
+        refreshList(page);
+    };
+
+    updatePage()
 }
+
 const getUrlWithPage = (page: number) => {
     const url = new URL(location.href);
 
-    if (!url.search.match("page=")) {
+    if (page === 1) {
+        url.search = url.search.replace(/&?page=\d+/, "");
+    }
+    else if (!url.search.match("page=")) {
         url.search += (url.search ? "&" : "") + "page=" + page;
     }
     else {
