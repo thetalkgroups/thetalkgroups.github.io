625876be01c0bb1763dbfb90ed8be75af961b04d
Mathias Hansen <mathias.hansen783@gmail.com>
2016-10-22 19:32:08 +0200

    updated sw

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
-// 1
+// 0
 
 importScripts('sw-toolbox.js');
+
+toolbox.options.debug = true;
 
 self.addEventListener("install", event => {
     self.skipWaiting();
@@ -8,14 +10,22 @@ self.addEventListener("install", event => {
 
 toolbox.precache([
     "/sign-in.html",
-    "/admin.html",
+    "/admin",
+    "/admin/",
+    "/admin/index.html",
 
+    "/thc/forum",
+    "/thc/forum/",
+    "/thc/forum/index.html",
     "/thc/forum/questions/add-item.html",
     "/thc/forum/questions/index.html",
     "/thc/forum/questions/item.html",
     "/thc/forum/tips-and-tricks/add-item.html",
     "/thc/forum/tips-and-tricks/index.html",
     "/thc/forum/tips-and-tricks/item.html",
+    // "/thc/forum/trip-reports/add-item.html",
+    // "/thc/forum/trip-reports/index.html",
+    // "/thc/forum/trip-reports/item.html",
     
     "/assets/static/facebook-logo.svg",
     "/assets/static/google-logo.svg",
@@ -35,31 +45,37 @@ toolbox.precache([
     "/css/list.css",
     "/css/sign-in.css",
 
-    "/js/add-item.roll.min.js",
-    "/js/common.roll.min.js",
-    "/js/item.roll.min.js",
-    "/js/list.roll.min.js",
-    "/js/sign-in.roll.min.js"
+    "/js/add-item.min.js",
+    "/js/common.min.js",
+    "/js/item.min.js",
+    "/js/list.min.js",
+    "/js/sign-in.min.js"
 ])
 
+const limitedCache = (request, values, options) => {
+    if (toolbox.options.debug) console.log("[custom] limitedCache", request.url);
 
-toolbox.router.get("/", toolbox.networkOnly)
-toolbox.router.get("/index.html", toolbox.networkOnly)
-toolbox.router.get("/*", toolbox.networkOnly)
-toolbox.router.get("/*/", toolbox.networkOnly)
-toolbox.router.get("/*/index.html", toolbox.networkOnly)
-toolbox.router.get("/*/general-hr.html", toolbox.networkOnly)
-toolbox.router.get("/*/test-kits.html", toolbox.networkOnly)
-toolbox.router.get("/*/forum", toolbox.networkOnly)
-toolbox.router.get("/*/forum/", toolbox.networkOnly)
-toolbox.router.get("/*/forum/index.html", toolbox.networkOnly)
-toolbox.router.get("/*/forum/harm-reduction.html", toolbox.networkOnly)
-toolbox.router.get("/*/forum/read-first.html", toolbox.networkOnly)
+    return toolbox.cacheFirst(request, values, { maxAgeSecound: 1800 });
+}
+
+const networkOnlyPaths = [
+    "/",
+    "/index.html",
+
+    "/(thc)",
+    "/(thc)/index.html",
+    "/(thc)/general-hr.html",
+    "/(thc)/test-kits.html",
+    "/(thc)/forum/harm-reduction.html",
+    "/(thc)/forum/read-first.html"
+]
+
+networkOnlyPaths.forEach(path => toolbox.router.get(path, limitedCache));
 
 toolbox.router.any("*", toolbox.networkOnly, { origin: "http://83.93.98.21:8000" });
 
-toolbox.router.get("*", toolbox.cacheFirst, { origin: "https://cdnjs.cloudflare.com" });
-toolbox.router.get("*", toolbox.cacheFirst, { origin: "https://www.gstatic.com" });
-toolbox.router.get("https://www.google-analytics.com/analytics.js", toolbox.cacheFirst);
-
 toolbox.router.get("*", toolbox.cacheFirst);
+toolbox.router.get("*", toolbox.cacheFirst, { origin: "https://www.gstatic.com" });
+toolbox.router.get("*", toolbox.cacheFirst, { origin: "https://cdnjs.cloudflare.com" });
+toolbox.router.get("*", toolbox.cacheFirst, { origin: "https://lh4.googleusercontent.com" });
+toolbox.router.get("https://www.google-analytics.com/analytics.js", toolbox.cacheFirst);
